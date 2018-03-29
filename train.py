8978cc6620c83adb87513193aabdec9ec5b61a30
Mark Lavrentyev <lavrema@outlook.com>
2018-03-28 23:02:12 -0400

    Change fcl weight initialization to xavier method

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ def conv_layer(input, channels_in, channels_out, filter_shape, name="conv"):
                                             stddev=0.1), name="W")
         b = tf.Variable(tf.constant(0., shape=[channels_out]), name="b")
         conv = tf.nn.conv2d(input, W, strides=[1, 1, 1, 1], padding="SAME")
-        activation = tf.nn.relu(conv + b, name="act")
+        activation = tf.nn.leaky_relu(conv + b, 0.1, name="act")
 
         tf.summary.histogram("weights", W)
         tf.summary.histogram("biases", b)
@@ -20,33 +20,35 @@ def conv_layer(input, channels_in, channels_out, filter_shape, name="conv"):
 
 def fc_layer(input, channels_in, channels_out, name="fcl"):
     with tf.name_scope(name):
-        W = tf.Variable(tf.truncated_normal(shape=[channels_in, channels_out], stddev=0.1), name="W")
+        initializer = tf.contrib.layers.xavier_initializer()
+        W = tf.Variable(initializer([channels_in, channels_out]), name="W")
         b = tf.Variable(tf.constant(0., shape=[channels_out]), name="b")
         ff = tf.matmul(input, W) + b
         activation = tf.sigmoid(ff, name="act")
+        dropout = tf.nn.dropout(activation, 0.5, name="dropout")
 
         tf.summary.histogram("weights", W)
         tf.summary.histogram("biases", b)
         tf.summary.histogram("act", activation)
+        tf.summary.histogram("dropout", dropout)
 
-    return activation
+    return dropout
 
 
-def neural_network(in_data, labels):
-    conv1 = conv_layer(in_data, 3, 32, (6,6), name="conv1")
+def neural_network(in_data):
+    conv1 = conv_layer(in_data, 3, 32, (6, 6), name="conv1")
     pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-    conv2 = conv_layer(pool1, 32, 64, (6, 6), name="conv2")
+    conv2 = conv_layer(pool1, 32, 32, (6, 6), name="conv2")
     pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
     # in_fcl_size = pool2.reduce_prod()
-    flattened = tf.reshape(pool2, [-1, 8*8*64], name="flattened")
+    flattened = tf.reshape(pool2, [-1, 8*8*32], name="flattened")
 
-    fcl1 = fc_layer(flattened, 8*8*64, 1024, name="fcl1")
-    fcl2 = fc_layer(fcl1, 1024, 2048, name="fcl2")
-    logits = fc_layer(fcl2, 2048, 10, name="logits")
+    fcl1 = fc_layer(flattened, 8*8*32, 1024, name="fcl1")
+    fcl2 = fc_layer(fcl1, 1024, 10, name="fcl2")
 
-    return logits
+    return fcl2
 
 
 def main(unused_argv):
@@ -73,14 +75,14 @@ def main(unused_argv):
     print("Data loaded")
     x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3], name="x")
     y = tf.placeholder(tf.float32, shape=[None, 10], name="labels")
-    logits = neural_network(x, y)
+    logits = neural_network(x)
 
     # Backpropogate to optimize
     with tf.name_scope("xent"):
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y))
         tf.summary.scalar("xent", cross_entropy)
     with tf.name_scope("train"):
-        optimizer = tf.train.AdamOptimizer(1e-4)
+        optimizer = tf.train.AdamOptimizer(6e-4)
         train_step = optimizer.minimize(cross_entropy)
         tf.summary.scalar("learning_rate", optimizer._lr)
     with tf.name_scope("accuracy"):
@@ -113,12 +115,10 @@ def main(unused_argv):
         writer.add_summary(s, i)
 
         # Print out accuracy
-        if i % 10 == 0:
+        if i % 100 == 0:
             train_accuracy = sess.run(accuracy, feed_dict=feed_dict)
             print("Step: %d, Training accuracy: %.2f" % (i, train_accuracy))
             saver.save(sess, trained_model_root_path + str(file_n) + "/model", global_step = i)
-        else:
-            print("Step: %d" % (i))
 
         sess.run(train_step, feed_dict=feed_dict)
 
